b495201c25f4df96c991da24920f897d27f8a451
steveweon130 <59664586+steveweon130@users.noreply.github.com>
2021-11-23 10:05:01 -0800

    Add files via upload

--- HackmanProject_SteveWeon_1-1/Puzzle.java
+++ HackmanProject_SteveWeon_1-1/Puzzle.java
@@ -9,10 +9,14 @@ public class Puzzle {
    private String guess, word, input, sum, temp, previous;
    private char[] blank, wordFound, wordAlrUsed;
    private boolean have, noInput, symbols, started, alreadyUsed, twoLetter;
-    
+   private String[] wordList = {"affix", "avenue", "awkward", "beekeeper", "boggle", "cobweb", "cycle", "disavow", "duplex", 
+                                "equip", "exodus", "funny", "galaxy", "gossip", "icebox", "injury", "ivory", "jackpot", "jelly",
+                                "jockey", "joking", "joyful", "jumbo", "kayak", "khaki", "kiosk", "lengths", "lucky", "luxury",
+                                "lymph", "nightclub", "onyx", "ovary", "pajama", "pneumonia", "pshaw", "puppy", "scratch", "staff", "stretch"};
+
    public Puzzle() {
        correct = index = 0;
-       input = "pneumonia";
+       input = wordList[(int)(Math.random()*wordList.length-1)];
        word = input.toUpperCase();
        wordFound = word.toCharArray();
        guess = sum = temp = previous = "";
